b67db7c71fc9f477b7edc7d3f63199094b2dfa28
Aman Jain <aman.codes0@gmail.com>
2023-11-08 20:37:24 +0530

    update cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: [
+      "https://nest-react-chat-app-server.vercel.app/",
       "http://localhost:3000",
       "http://localhost:8080",
       "http://localhost:4200",
