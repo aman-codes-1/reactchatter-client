87c6ebad606df9bcdd5f234bd1ea56b9c1b463ce
Aman Jain <aman.codes0@gmail.com>
2023-11-08 20:42:47 +0530

    update cors

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -17,8 +17,8 @@ export class PusherService {
       appId: '1701877',
       key: '58b0ec4905821bf0b6de',
       secret: '075286a370da33dbcf4d',
-      cluster: 'ap2',
-      useTLS: true,
+      cluster: 'ap2', 
+      // useTLS: true,
     });
     await pusher.trigger(channel, event, data);
   }
