f37a7d91a3221c2bffb0a24c7bdbb5684c86f496
Aman Jain <aman.codes0@gmail.com>
2023-11-08 17:59:01 +0530

    update providers

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppService, PusherService } from './app.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PusherService],
 })
 export class AppModule {}
