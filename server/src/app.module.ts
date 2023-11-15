b55871127575159a988f6c5e96d57579759b9b0d
Aman Jain <aman.codes0@gmail.com>
2023-11-15 06:45:53 +0530

    complete google login

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
-import { MongooseModule } from "@nestjs/mongoose";
-import { User, UserSchema } from "./user.schema";
+// import { MongooseModule } from "@nestjs/mongoose";
+// import { User, UserSchema } from "./user.schema";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { AuthController } from "./auth.controller";
-import { AuthService } from "./auth.service";
+// import { AuthService } from "./auth.service";
 import { PusherController } from "./pusher.controller";
 import { PusherService } from "./pusher.service";
 // import 'dotenv/config';
