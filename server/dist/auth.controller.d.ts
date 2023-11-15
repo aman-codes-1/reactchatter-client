556906ed5652e740d8424e3b987e01e27a6a20d0
Aman Jain <aman.codes0@gmail.com>
2023-11-15 05:33:34 +0530

    complete google login

--- server/dist/auth.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { AuthService } from "./auth.service";
-import 'dotenv/config';
-export declare class AuthController {
-    private authService;
-    constructor(authService: AuthService);
-    login(token: string): Promise<any>;
-}
--- server/dist/pusher.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { PusherService } from "./pusher.service";
-export declare class PusherController {
-    private pusherService;
-    constructor(pusherService: PusherService);
-    messages(username: string, message: string): Promise<any[]>;
-}
