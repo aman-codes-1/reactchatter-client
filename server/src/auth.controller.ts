9fd4e5dc4071fd52870233c91b6d5213f9a5eec2
Aman Jain <aman.codes0@gmail.com>
2023-11-15 06:40:42 +0530

    complete google login

--- server/src/auth.controller.ts
+++ server/src/auth.controller.ts
@@ -3,16 +3,17 @@ import { OAuth2Client } from "google-auth-library";
 import { AuthService } from "./auth.service";
 import "dotenv/config";
 
+export const client = new OAuth2Client({
+  clientId: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+});
+
 @Controller("auth")
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post("login")
   async login(@Body("token") token: string): Promise<any> {
-    const client = new OAuth2Client({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    });
     const ticket = await client.verifyIdToken({
       idToken: token,
       audience: process.env.GOOGLE_CLIENT_ID,
