ebfc9f0b83a72967546c306723b168df3f4c75ad
Aman Jain <aman.codes0@gmail.com>
2023-12-22 12:28:34 +0530

    fix cors error

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,14 +3,15 @@ import cookieParser from "cookie-parser";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: process.env.ALLOWED_DOMAINS?.includes(",")
-      ? process.env.ALLOWED_DOMAINS?.split(",")
-      : process.env.ALLOWED_DOMAINS,
-    allowedHeaders:
-      "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe",
-    credentials: true,
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: process.env.ALLOWED_DOMAINS?.includes(",")
+        ? process.env.ALLOWED_DOMAINS?.split(",")
+        : process.env.ALLOWED_DOMAINS,
+      allowedHeaders:
+        "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe",
+      credentials: true,
+    },
   });
   app.use(cookieParser());
   await app.listen(8000);
