af39e1cb78065dc7ce3fe5e125322604b3a11155
Aman Jain <aman.codes0@gmail.com>
2023-12-24 02:39:50 +0530

    fix cors error

--- server/dist/friend.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { FriendService } from "./friend.service";
-export declare class FriendController {
-    private friendService;
-    constructor(friendService: FriendService);
-    allFriends(userId: string): Promise<{
-        data: any;
-        message: string;
-    }>;
-}
--- server/dist/pusher.service.d.ts
+++
@@ -1,7 +0,0 @@
-declare var Pusher: any;
-export declare class PusherService {
-    pusher: typeof Pusher;
-    constructor();
-    trigger(channel: string, event: string, data: any): Promise<void>;
-}
-export {};
