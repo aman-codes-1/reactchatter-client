27be464fa9507c6a2517c2506d60eda35e5c4596
Aman Jain <aman.codes0@gmail.com>
2024-06-23 19:35:55 +0530

    fix nginx.conf

--- nginxPop.sh
+++ nginxPop.sh
@@ -2,11 +2,10 @@
 
 # Export environment variables to make them available to envsubst
 export PORT="$PORT"
-export REACT_APP_URI="$REACT_APP_URI"
 export REACT_APP_SERVER_URI="$REACT_APP_SERVER_URI"
 
 # Use envsubst to replace variables directly in nginx.conf and create a temporary file
-envsubst '$PORT $REACT_APP_URI $REACT_APP_SERVER_URI' < /etc/nginx/nginx.conf > /etc/nginx/nginx.conf.tmp
+envsubst '$PORT $REACT_APP_SERVER_URI' < /etc/nginx/nginx.conf > /etc/nginx/nginx.conf.tmp
 
 # Move the temporary file to replace the original nginx.conf
 mv /etc/nginx/nginx.conf.tmp /etc/nginx/nginx.conf
