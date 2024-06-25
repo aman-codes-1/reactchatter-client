a7f9ffde19408609ca4747cb125f9fa9f2d9e456
Aman Jain <aman.codes0@gmail.com>
2024-06-25 14:31:59 +0530

    add middleware.js file

---
+++ middleware.js
@@ -0,0 +1,29 @@
+/* eslint-disable consistent-return */
+import { rewrite } from '@vercel/edge';
+
+export default function middleware(request) {
+  const url = new URL(request.url);
+
+  const serverUri = process.env.REACT_APP_SERVER_URI;
+  const serverDomain = process.env.REACT_APP_SERVER_DOMAIN;
+
+  if (url.pathname === '/api') {
+    return rewrite(new URL(`${serverUri}/api`, request.url));
+  }
+
+  if (url.pathname.startsWith('/api/')) {
+    return rewrite(new URL(`${serverUri}${url.pathname}`, request.url));
+  }
+
+  if (url.pathname.startsWith('/socket.io/')) {
+    return rewrite(new URL(`${serverUri}/socket.io/`, request.url));
+  }
+
+  if (url.pathname.startsWith('/graphql')) {
+    return rewrite(new URL(`${serverUri}/graphql`, request.url));
+  }
+
+  if (url.pathname.startsWith('/ws/graphql')) {
+    return rewrite(new URL(`wss://${serverDomain}/graphql`, request.url));
+  }
+}
