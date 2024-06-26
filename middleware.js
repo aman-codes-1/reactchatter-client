345d08ca477179cf40b9d34907d502cbdc88444d
Aman Jain <aman.codes0@gmail.com>
2024-06-26 15:29:58 +0530

    add middleware.js test ws

---
+++ middleware.js
@@ -0,0 +1,38 @@
+import { next, rewrite } from '@vercel/edge';
+
+export default function middleware(request) {
+  const url = new URL(request.url);
+
+  const serverUri = process.env.REACT_APP_SERVER_URI;
+
+  if (!serverUri) {
+    return next();
+  }
+
+  const upgradeHeader = request.headers.get('upgrade');
+
+  if (url.pathname === '/api') {
+    return rewrite(new URL(`${serverUri}/api`, request.url));
+  }
+
+  if (url.pathname.startsWith('/api/')) {
+    return rewrite(
+      new URL(`${serverUri}${url.pathname}${url.search}`, request.url),
+    );
+  }
+
+  if (url.pathname.startsWith('/graphql')) {
+    if (upgradeHeader && upgradeHeader.toLowerCase() === 'websocket') {
+      return rewrite(
+        new URL(`${serverUri.replace(/^http/, 'ws')}/graphql`, request.url),
+      );
+    }
+    return rewrite(new URL(`${serverUri}/graphql`, request.url));
+  }
+
+  return next();
+}
+
+export const config = {
+  matcher: ['/api', '/api/*', '/graphql', '/graphql/*'],
+};
