a42e0a58b16c6f7db39d7f5fa21ad3fd85dc3471
Aman Jain <aman.codes0@gmail.com>
2024-06-29 19:04:57 +0530

    fix graphql-ws proxy and auth loader

--- nginxPop.sh
+++ nginxPop.sh
@@ -4,10 +4,9 @@
 export PORT="$PORT"
 export REACT_APP_PROXY_URI="$REACT_APP_PROXY_URI"
 export REACT_APP_SERVER_URI="$REACT_APP_SERVER_URI"
-export REACT_APP_SERVER_DOMAIN="$REACT_APP_SERVER_DOMAIN"
 
 # Use envsubst to replace variables directly in nginx.conf and create a temporary file
-envsubst '$PORT $REACT_APP_PROXY_URI $REACT_APP_SERVER_URI $REACT_APP_SERVER_DOMAIN' < /etc/nginx/nginx.conf > /etc/nginx/nginx.conf.tmp
+envsubst '$PORT $REACT_APP_PROXY_URI $REACT_APP_SERVER_URI' < /etc/nginx/nginx.conf > /etc/nginx/nginx.conf.tmp
 
 # Move the temporary file to replace the original nginx.conf
 mv /etc/nginx/nginx.conf.tmp /etc/nginx/nginx.conf
