eda98045d4a5eabfc7917790c733a7bf56c01cb0
Aman Jain <aman.codes0@gmail.com>
2024-06-29 16:59:04 +0530

    fix graphql-ws proxy and auth loader

--- src/hooks/useNavigatorOnLine.ts
+++ src/hooks/useNavigatorOnLine.ts
@@ -1,4 +1,4 @@
-import { useLayoutEffect, useState } from 'react';
+import { useLayoutEffect, useRef, useState } from 'react';
 
 export const useNavigatorOnLine = () => {
   const url = 'https://www.google.com';
@@ -6,6 +6,7 @@ export const useNavigatorOnLine = () => {
   const urlTimeout = 5000;
   const [isLoading, setIsLoading] = useState(true);
   const [isOffline, setIsOffline] = useState(false);
+  const hasFetched = useRef(false);
 
   useLayoutEffect(() => {
     let controllerTimeout: any = 0;
@@ -16,7 +17,9 @@ export const useNavigatorOnLine = () => {
       setIsOffline(false);
       try {
         await fetch(url, {
-          mode: 'no-cors',
+          ...(process.env.NODE_ENV === 'development'
+            ? { mode: 'no-cors' }
+            : {}),
           signal: controller.signal,
         });
         setIsOffline(false);
@@ -28,6 +31,8 @@ export const useNavigatorOnLine = () => {
       }
     };
 
+    if (hasFetched.current) return;
+
     let interval: any;
     if (isOffline) {
       interval = setInterval(() => {
@@ -37,8 +42,12 @@ export const useNavigatorOnLine = () => {
     } else {
       fetchRequest();
     }
+
     window.addEventListener('online', fetchRequest);
     window.addEventListener('offline', fetchRequest);
+
+    hasFetched.current = true;
+
     return () => {
       window.removeEventListener('online', fetchRequest);
       window.removeEventListener('offline', fetchRequest);
