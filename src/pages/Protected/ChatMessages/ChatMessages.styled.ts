5d33f37424942769e9c53fb9a974435806c63c5a
Aman Jain <aman.codes0@gmail.com>
2024-07-03 19:46:26 +0530

    small ui changes

--- src/pages/Protected/ChatMessages/ChatMessages.styled.ts
+++ src/pages/Protected/ChatMessages/ChatMessages.styled.ts
@@ -9,7 +9,7 @@ export const ChatMessagesStyled = styled('div')<{
   '.chat-container': {
     display: 'flex',
     width: '100%',
-    height: '100%',
+    height: '100svh',
     overflow: 'auto',
     padding: '0rem 2.5rem',
     maxHeight: `calc(100svh - ${textFieldHeight || 0}px - 2rem)`,
