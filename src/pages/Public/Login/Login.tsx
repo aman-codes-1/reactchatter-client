ba7e22ea320bbcc9103af5e7f6b085029019b6b1
Aman Jain <aman.codes0@gmail.com>
2024-07-09 22:53:37 +0530

    fix nginx http2_max

--- src/pages/Public/Login/Login.tsx
+++ src/pages/Public/Login/Login.tsx
@@ -1,9 +1,11 @@
-import { useLayoutEffect } from 'react';
+import { useLayoutEffect, useState } from 'react';
 import { useLocation, useNavigate, useSearchParams } from 'react-router-dom';
 import { GoogleLogin } from '@react-oauth/google';
 import { jwtDecode } from 'jwt-decode';
-import { Typography } from '@mui/material';
+import { CircularProgress, Typography } from '@mui/material';
+import { LoadingButton } from '@mui/lab';
 import { useAuth, useSnackbar } from '../../../hooks';
+import { BaseSvg } from '../../../components';
 import { LoginStyled } from './Login.styled';
 
 const Login = () => {
@@ -15,11 +17,13 @@ const Login = () => {
   const [searchParams] = useSearchParams();
   const token = searchParams.get('token');
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
   const { setAuth } = useAuth();
   const { openSnackbar } = useSnackbar();
 
   useLayoutEffect(() => {
     const googleLogin = async () => {
+      setIsLoading(true);
       const decoded = jwtDecode(token || '');
       localStorage.setItem('token', token || '');
       setAuth({
@@ -27,6 +31,7 @@ const Login = () => {
         ...decoded,
       });
       navigate('/', { replace: true });
+      setIsLoading(false);
     };
     if (token) {
       googleLogin();
@@ -54,13 +59,32 @@ const Login = () => {
       <Typography className="sign-in-heading" fontWeight={700}>
         Sign in to your account
       </Typography>
-      <GoogleLogin
-        // useOneTap
-        ux_mode="redirect"
-        shape="pill"
-        click_listener={handleLogin}
-        onSuccess={() => {}}
-      />
+      {!isLoading ? (
+        <GoogleLogin
+          // useOneTap
+          ux_mode="redirect"
+          shape="pill"
+          click_listener={handleLogin}
+          onSuccess={() => {}}
+        />
+      ) : (
+        <LoadingButton
+          className={`google-login-btn ${
+            isLoading ? 'btn-disabled' : 'btn-active'
+          }`}
+          loading={isLoading}
+          variant="contained"
+          startIcon={
+            isLoading ? (
+              <CircularProgress className="loading-indicator" size={18} />
+            ) : (
+              <BaseSvg id="google-logo" width="18" height="18" />
+            )
+          }
+        >
+          Signing in with Google
+        </LoadingButton>
+      )}
     </LoginStyled>
   );
 };
