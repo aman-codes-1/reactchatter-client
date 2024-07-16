fd36ac23c20f0894d1203688fcc5f702574caef8
Aman Jain <aman.codes0@gmail.com>
2024-07-17 02:59:28 +0530

    fix Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -6,13 +6,11 @@ ARG PORT
 ARG REACT_APP_PROXY_URI
 ARG REACT_APP_SERVER_URI
 ARG REACT_APP_PROXY_DOMAIN
-ARG REACT_APP_GOOGLE_CLIENT_ID
 
 ENV PORT=$PORT
 ENV REACT_APP_PROXY_URI=$REACT_APP_PROXY_URI
 ENV REACT_APP_SERVER_URI=$REACT_APP_SERVER_URI
 ENV REACT_APP_PROXY_DOMAIN=$REACT_APP_PROXY_DOMAIN
-ENV REACT_APP_GOOGLE_CLIENT_ID=$REACT_APP_GOOGLE_CLIENT_ID
 
 # Create app directory and copy files
 WORKDIR /app
@@ -30,7 +28,7 @@ FROM nginx:alpine
 COPY --from=build /app/build /usr/share/nginx/html
 
 # Copy the custom Nginx configuration file to the container
-COPY nginx.conf /etc/nginx/nginx.conf
+COPY nginx.conf /etc/nginx/conf.d/default.conf
 
 # Copy the entry point script into the container
 COPY nginxPop.sh /nginxPop.sh
--- nginxPop.sh
+++ nginxPop.sh
@@ -6,11 +6,11 @@ export REACT_APP_PROXY_URI="$REACT_APP_PROXY_URI"
 export REACT_APP_SERVER_URI="$REACT_APP_SERVER_URI"
 export REACT_APP_PROXY_DOMAIN="$REACT_APP_PROXY_DOMAIN"
 
-# Use envsubst to replace variables directly in nginx.conf and create a temporary file
-envsubst '$PORT $REACT_APP_PROXY_URI $REACT_APP_SERVER_URI $REACT_APP_PROXY_DOMAIN' < /etc/nginx/nginx.conf > /etc/nginx/nginx.conf.tmp
+# Use envsubst to replace variables directly in default.conf and create a temporary file
+envsubst '$PORT $REACT_APP_PROXY_URI $REACT_APP_SERVER_URI $REACT_APP_PROXY_DOMAIN' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf.tmp
 
-# Move the temporary file to replace the original nginx.conf
-mv /etc/nginx/nginx.conf.tmp /etc/nginx/nginx.conf
+# Move the temporary file to replace the original default.conf
+mv /etc/nginx/conf.d/default.conf.tmp /etc/nginx/conf.d/default.conf
 
 # Start Nginx with the provided command line arguments
 exec "$@"
