3aa19de05deba94b323bce2a81bf389b07945190
aman-codes-1 <aman.codes0@gmail.com>
2024-10-05 01:29:46 +0530

    updated npm packages, migrated eslint to v9

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,33 +1,30 @@
-import { fixupConfigRules, fixupPluginRules } from '@eslint/compat';
-import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import typescriptEslint from 'typescript-eslint';
 import react from 'eslint-plugin-react';
 import prettier from 'eslint-plugin-prettier';
 import globals from 'globals';
 import tsParser from '@typescript-eslint/parser';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 import js from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
+import importPlugin from 'eslint-plugin-import';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
+const languageOptions = {
+  globals: {
+    ...globals.node,
+    ...globals.jest,
+  },
+  ecmaVersion: 'latest',
+  sourceType: 'module',
+  parser: tsParser,
+};
+
+const plugins = {
+  import: importPlugin,
+  'import/parsers': tsParser,
+  react,
+  prettier,
+};
 
 export default [
-  ...fixupConfigRules(
-    compat.extends(
-      'eslint:recommended',
-      'plugin:react/recommended',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:import/errors',
-      'plugin:import/warnings',
-      'prettier',
-    ),
-  ),
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
   {
     ignores: [
       'node_modules/**/*',
@@ -38,45 +35,16 @@ export default [
     ],
   },
   {
-    plugins: {
-      '@typescript-eslint': fixupPluginRules(typescriptEslint),
-      react: fixupPluginRules(react),
-      prettier,
-    },
-
+    plugins,
+  },
+  {
     languageOptions: {
-      globals: {
-        ...globals.browser,
-      },
-
-      parser: tsParser,
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
+      ...languageOptions,
     },
-
-    settings: {
-      react: {
-        createClass: 'createReactClass',
-        pragma: 'React',
-        fragment: 'Fragment',
-        version: 'detect',
-        flowVersion: '0.53',
-      },
-      'import/resolver': {
-        typescript: {},
-
-        node: {
-          extensions: ['.js', '.jsx', '.ts', '.tsx'],
-        },
-      },
-    },
-
+  },
+  js.configs.recommended,
+  ...typescriptEslint.configs.recommended,
+  {
     rules: {
       'prettier/prettier': ['error'],
       'import/no-extraneous-dependencies': 'off',
@@ -93,26 +61,7 @@ export default [
         },
       ],
 
-      'import/extensions': [
-        'error',
-        'ignorePackages',
-        {
-          ts: 'never',
-          tsx: 'never',
-          js: 'never',
-          jsx: 'never',
-        },
-      ],
-
-      'import/no-unresolved': [
-        2,
-        {
-          commonjs: true,
-          amd: true,
-        },
-      ],
-
-      'import/named': 2,
+      'import/named': 'off',
       'import/namespace': 0,
       'import/default': 2,
       'import/export': 2,
